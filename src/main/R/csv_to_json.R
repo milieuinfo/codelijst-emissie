5b06e5ae4707b70b7b29cea472b373408055ffdb
gezever <gezever@skynet.be>
2022-08-31 15:59:38 +0200

    topconcept

--- src/main/R/csv_to_json.R
+++ src/main/R/csv_to_json.R
@@ -5,7 +5,12 @@ library(jsonlite)
 setwd("/home/gehau/git/codelijst-emissie/src/main/R")
 
 df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/conceptscheme/emissie/emissie.csv", sep=",", na.strings=c("","NA"))
-
+tco <- subset(df, topConceptOf == 'https://data.omgeving.vlaanderen.be/id/conceptscheme/emissie' ,
+              select=c(uri, topConceptOf))
+htc <- as.list(tco["uri"])
+df2 <- data.frame('https://data.omgeving.vlaanderen.be/id/conceptscheme/emissie', htc)
+names(df2) <- c("uri","hasTopConcept")
+df <- bind_rows(df, df2)
 df <- df %>%
   rename("@id" = uri,
          "@type" = type)
