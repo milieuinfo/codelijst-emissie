9edfd311cee7f929a3af7c75cfa76040a9545ce9
gezever <gezever@skynet.be>
2022-09-07 14:24:08 +0200

    setwd verwijderd

--- src/main/R/csv_to_json.R
+++ src/main/R/csv_to_json.R
@@ -2,7 +2,6 @@
 library(tidyr)
 library(dplyr)
 library(jsonlite)
-setwd("/home/gehau/git/codelijst-emissie/src/main/R")
 
 df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/conceptscheme/emissie/emissie.csv", sep=",", na.strings=c("","NA"))
 tco <- subset(df, topConceptOf == 'https://data.omgeving.vlaanderen.be/id/conceptscheme/emissie' ,
